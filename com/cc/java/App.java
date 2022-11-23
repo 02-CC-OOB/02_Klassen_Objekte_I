4417ca503187d173ebaebc455e5edb0afae591d8
Peter <schmidt.multimedia@gmail.com>
2022-11-23 11:53:33 +0100

    private / getter / setter

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -3,26 +3,25 @@ package com.cc.java;
 public class App {
     
     public static void main(String[] args) {
-
     
         Cat cat = new Cat("Grizabella","white",29);
-       
-        output(cat.tellYourName());
-        output(cat.tellYourFurColor());
-        output(String.valueOf(cat.tellYourAge()));
+        output(cat.getName());
+        output(cat.getFurColor());
+        output(String.valueOf(cat.getAge()));
     
         output("------------------------");
 
        Cat cat1 = new Cat("Alonzo", "grey", 35);
 
-        output(cat1.tellYourName());
-        output(cat1.tellYourFurColor());
-        output(String.valueOf(cat1.tellYourAge()));
+       cat1.setFurColor("striped");
 
+        output(cat1.getName());
+        output(cat1.getFurColor());
+        output(String.valueOf(cat1.getAge()));
 
     }
 
-    // Methode witrd aus der KLASSE aufgerufen, daher static
+    // Methode wird aus der KLASSE aufgerufen, daher static
     public static void output(String outputStr) {
         System.out.println(outputStr);
     }
